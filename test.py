66feaae4881078458c4692a7c05c2e76e149d084
dongcheng <donnecheng@gmail.com>
2015-12-25 17:54:08 +0800

    提交学习代码

--- test.py
+++ test.py
@@ -1,2 +1,46 @@
-p
-p
+#打印计算
+print('100 + 200 =', 100 + 200)
+
+#打印输入
+name = input()
+print('hello, your name is \'', name, '\'')
+
+#打印计算
+print(3>2)
+print(10/3)
+print(10//3)
+print(10%3)
+
+#判断
+a = 100
+if a >= 0:
+	print(a)
+else:
+	print(-a)
+
+#输入数字转换
+s = input('birth:')
+birth = int(s)
+if birth < 2000:
+	print('00前')
+else:
+	print('00后')
+
+#列表生成
+print(list(range(10)))
+
+#生成器
+g = (x * x for x in range(10))
+for n in g:
+	print(n)
+
+#斐波拉切数列
+def fib(max):
+	n, a, b = 0, 0, 1
+	while n < max:
+		print(b)
+		a, b = b, a + b
+		n = n + 1
+	return 'done'
+
+print(fib(12))
