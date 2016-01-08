239b948107b45b1245d891c3a986bae1f6786870
dongcheng <donnecheng@gmail.com>
2016-01-08 16:55:21 +0800

    add write file

--- applog-preprocess/readlog.py
+++ applog-preprocess/readlog.py
@@ -1,16 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import redis
 import json
 from pprint import pprint
+import time
 
-r = redis.Redis(host='172.30.74.194',port=6379,db=1)
+keys = ["app_key", "channel_id", "channel_num", "channel_title", "client_code", 
+	"client_ver", "ctime", "destation", "device_model", "device_os", 
+	"device_product", "device_size", "endtime", "event_name", 
+	"identifier", "ip",
+	"isoCC", "isoCe", "latitude", "longitude", "MCC", "message", "MNC", 
+	"network_state", "news_id", "news_title", "platform", "source", "sp", 
+	"startup", "state", "tags", "type", "udid", "user_id", "user_name", 
+	"user_type", "visit_id", "visit_start_time", "deal_date"]
 
-while 1:
-	log = r.rpop('test_log')
-	if(log):
-		data = json.loads(log.decode())
-		print(data['ctime'])
-		pprint(data)
-	else:
+r = redis.Redis(host='10.100.10.179',port=6379,db=0,password='0928.7NAhTH7UnCxyd9.2015')
+print(r.llen('apilog:ad'))
+
+i = 0
+flag = True
+while  flag:
+	start =  i * 1000
+	end = start + 999
+	logs = r.lrange("apilog:ad",start,end)
+	if (logs == None  or len(logs) == 0):
+		flag = False
 		break
+	for x in logs:
+		try:
+			data = json.loads(x.decode('utf-8'))
+			with open('/home/cheng/adlog6.txt', 'a') as f:
+				for key in keys:
+					if(key == 'ctime'):
+						timearray = time.localtime(int(data.get(key)))
+						f.write(time.strftime("%Y-%m-%d %H:%M:%S", timearray))
+					else:
+						f.write(str(data.get(key)))
+					f.write("\t")
+				f.write('\n')
+		except ValueError as e:
+			print(x.decode('utf-8'))
+			continue
+		
+	print(i * 1000)
+	i=i+1
 
 print('处理完成!')
--- routine/delete_es_index.py
+++ routine/delete_es_index.py
@@ -1,6 +1,7 @@
 import requests
 
 #error data not betweent 2013 ~ 2017
+
 years =  list(range(1970,2013)) +  list(range(2018,2039))
 
 for year in years:
